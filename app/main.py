f4fdfa2cf211f0ec0a5279c63943b9c346bf6344
Byron Kontou <byronkontou@gmail.com>
2019-03-02 11:15:36 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,215 +21,213 @@ class Queue:
         return obj
 
 
-# =============================================================================
-# 
-# 
-# class Loc:
-#     def __init__(self,x,y):
-#         self.x = x
-#         self.y = y
-#     
-#     def update_x(self,n):
-#         return Loc(self.x + n, self.y)
-#     
-#     def update_y(self,n):
-#         return Loc(self.x, self.y + n)
-#     
-#     def dist(self):
-#         return self.x**2 + self.y**2
-#     
-#     def __add__(self, L):
-#         return Loc(self.x + L.x, self.y + L.y)
-#     
-#     def __sub__(self, L):
-#         return Loc(self.x - L.x, self.y - L.y)
-#     
-#     def __str__(self):
-#         return("(%d,%d)"%(self.x,self.y))
-# 
-#     def __eq__(self, L):
-#         if self.x == L.x and self.y == L.y:
-#             return True
-#         else:
-#             return False
-#     
-# 
-# class Tree:
-#     def __init__(self, cargo, up=None, down=None, left=None, right=None, prev=None):
-#         self.cargo = cargo
-#         self.up = up
-#         self.down = down
-#         self.left = left
-#         self.right = right
-#         self.prev = prev
-#         
-#         
-# def search_tree(T_H):
-#     Q = Queue()
-#     Q.put(T_H)
-#     path = []
-#     path.append
-#     
-#     while not Q.empty():
-#         T = Q.get()
-#         
-#         if T.up:
-#             Q.put(T.up)
-#         if T.down:
-#             Q.put(T.down)
-#         if T.left:
-#             Q.put(T.left)
-#         if T.right:
-#             Q.put(T.right)
-#             
-# def BFS(T_H,l):
-#     Q = Queue()
-#     
-#     Q.put(T_H)
-#     
-#     while not Q.empty():
-#         T = Q.get()
-#         
-#         if T.cargo == l:
-#             #print("ladies and gentlemen... we got em")
-#             return get_path(T)
-#         
-#         if T.up:
-#             Q.put(T.up)
-#         if T.down:
-#             Q.put(T.down)
-#         if T.left:
-#             Q.put(T.left)
-#         if T.right:
-#             Q.put(T.right)
-#     
-#     return None
-# 
-# def get_path(T):
-#     path = []
-#     
-#     path.append(T.cargo)
-#     N = T.prev
-#     while N != None:
-#         path.append(N.cargo)
-#         N = N.prev
-#     
-#     return path
-# 
-#         
-# 
-# def build_tree(M,H_L):
-#     loc_list = []
-#             
-#     T_H = Tree(H_L)
-#     up = T_H.cargo.update_x(-1)
-#     down = T_H.cargo.update_x(1)
-#     left = T_H.cargo.update_y(-1)
-#     right = T_H.cargo.update_y(1)
-#     loc_list.append(T_H.cargo)
-#     
-#     Q = Queue()
-#     
-#     
-#     try:
-#         if M[up.x][up.y] != 1 and (up.x >= 0 and up.y >= 0):
-#             T_H.up = Tree(up, prev=T_H)
-#             Q.put(T_H.up)
-#             loc_list.append(up)
-#     except:
-#         pass
-#     try:
-#         if M[down.x][down.y] != 1 and (down.x >= 0 and down.y >= 0):
-#             T_H.down = Tree(down, prev=T_H)
-#             Q.put(T_H.down)
-#             loc_list.append(down)
-#     except:
-#         pass
-#     try:
-#         if M[left.x][left.y] != 1 and (left.x >= 0 and left.y >= 0):
-#             T_H.left = Tree(left, prev=T_H)
-#             Q.put(T_H.left)
-#             loc_list.append(left)
-#     except:
-#         pass
-#     try:
-#         if M[right.x][right.y] != 1 and (right.x >= 0 and right.y >= 0):
-#             T_H.right = Tree(right, prev=T_H)
-#             Q.put(T_H.right)
-#             loc_list.append(right)
-#     except:
-#         pass
-#     
-#     
-#     
-#     n = 0
-# 
-#     while not Q.empty():
-#         n+=1
-#         
-#         T = Q.get()
-#         
-#         up = T.cargo.update_x(-1)
-#         down = T.cargo.update_x(1)
-#         left = T.cargo.update_y(-1)
-#         right = T.cargo.update_y(1)
-#     
-#         try:
-#             if M[up.x][up.y] != 1 and (up.x >= 0 and up.y >= 0) and up not in loc_list:
-#                 T.up = Tree(up, prev=T)
-#                 Q.put(T.up)
-#                 loc_list.append(up)
-#         except:
-#             pass
-#             
-#         try:
-#             if M[down.x][down.y] != 1 and (down.x >= 0 and down.y >= 0) and down not in loc_list:
-#                 T.down = Tree(down, prev=T)
-#                 Q.put(T.down)
-#                 loc_list.append(down)
-#         except:
-#             pass
-#             
-#         try:
-#             if M[left.x][left.y] != 1 and (left.x >= 0 and left.y >= 0) and left not in loc_list:
-#                 T.left = Tree(left, prev=T)
-#                 Q.put(T.left)
-#                 loc_list.append(left)
-#         except:
-#             pass
-#             
-#         try:
-#             if M[right.x][right.y] != 1 and (right.x >= 0 and right.y >= 0) and right not in loc_list:
-#                 T.right = Tree(right, prev=T)
-#                 Q.put(T.right)
-#                 loc_list.append(right)
-#         except:
-#             pass
-#         
-#     return T_H
-#         
-#     
-# def to_loc_list(locs):
-#     loc_list = []
-#     
-#     for l in locs:
-#         loc_list.append(Loc(l['x'],l['y']))
-#         
-#     return loc_list
-#     
-#     
-#     
-# #       up        right    down     left
-# dirs = [Loc(-1,0),Loc(0,1),Loc(1,0),Loc(0,-1)]
-# global M
-# global H
-# global W
-# M = []
-# 
-# data = bottle.request.json
-#     
-# H, W = data["board"]["height"], data["board"]["height"]
-# =============================================================================
+
+
+class Loc:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def update_x(self,n):
+        return Loc(self.x + n, self.y)
+    
+    def update_y(self,n):
+        return Loc(self.x, self.y + n)
+    
+    def dist(self):
+        return self.x**2 + self.y**2
+    
+    def __add__(self, L):
+        return Loc(self.x + L.x, self.y + L.y)
+    
+    def __sub__(self, L):
+        return Loc(self.x - L.x, self.y - L.y)
+    
+    def __str__(self):
+        return("(%d,%d)"%(self.x,self.y))
+
+    def __eq__(self, L):
+        if self.x == L.x and self.y == L.y:
+            return True
+        else:
+            return False
+    
+
+class Tree:
+    def __init__(self, cargo, up=None, down=None, left=None, right=None, prev=None):
+        self.cargo = cargo
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+        self.prev = prev
+        
+        
+def search_tree(T_H):
+    Q = Queue()
+    Q.put(T_H)
+    path = []
+    path.append
+    
+    while not Q.empty():
+        T = Q.get()
+        
+        if T.up:
+            Q.put(T.up)
+        if T.down:
+            Q.put(T.down)
+        if T.left:
+            Q.put(T.left)
+        if T.right:
+            Q.put(T.right)
+            
+def BFS(T_H,l):
+    Q = Queue()
+    
+    Q.put(T_H)
+    
+    while not Q.empty():
+        T = Q.get()
+        
+        if T.cargo == l:
+            #print("ladies and gentlemen... we got em")
+            return get_path(T)
+        
+        if T.up:
+            Q.put(T.up)
+        if T.down:
+            Q.put(T.down)
+        if T.left:
+            Q.put(T.left)
+        if T.right:
+            Q.put(T.right)
+    
+    return None
+
+def get_path(T):
+    path = []
+    
+    path.append(T.cargo)
+    N = T.prev
+    while N != None:
+        path.append(N.cargo)
+        N = N.prev
+    
+    return path
+
+        
+
+def build_tree(M,H_L):
+    loc_list = []
+            
+    T_H = Tree(H_L)
+    up = T_H.cargo.update_x(-1)
+    down = T_H.cargo.update_x(1)
+    left = T_H.cargo.update_y(-1)
+    right = T_H.cargo.update_y(1)
+    loc_list.append(T_H.cargo)
+    
+    Q = Queue()
+    
+    
+    try:
+        if M[up.x][up.y] != 1 and (up.x >= 0 and up.y >= 0):
+            T_H.up = Tree(up, prev=T_H)
+            Q.put(T_H.up)
+            loc_list.append(up)
+    except:
+        pass
+    try:
+        if M[down.x][down.y] != 1 and (down.x >= 0 and down.y >= 0):
+            T_H.down = Tree(down, prev=T_H)
+            Q.put(T_H.down)
+            loc_list.append(down)
+    except:
+        pass
+    try:
+        if M[left.x][left.y] != 1 and (left.x >= 0 and left.y >= 0):
+            T_H.left = Tree(left, prev=T_H)
+            Q.put(T_H.left)
+            loc_list.append(left)
+    except:
+        pass
+    try:
+        if M[right.x][right.y] != 1 and (right.x >= 0 and right.y >= 0):
+            T_H.right = Tree(right, prev=T_H)
+            Q.put(T_H.right)
+            loc_list.append(right)
+    except:
+        pass
+    
+    
+    
+    n = 0
+
+    while not Q.empty():
+        n+=1
+        
+        T = Q.get()
+        
+        up = T.cargo.update_x(-1)
+        down = T.cargo.update_x(1)
+        left = T.cargo.update_y(-1)
+        right = T.cargo.update_y(1)
+    
+        try:
+            if M[up.x][up.y] != 1 and (up.x >= 0 and up.y >= 0) and up not in loc_list:
+                T.up = Tree(up, prev=T)
+                Q.put(T.up)
+                loc_list.append(up)
+        except:
+            pass
+            
+        try:
+            if M[down.x][down.y] != 1 and (down.x >= 0 and down.y >= 0) and down not in loc_list:
+                T.down = Tree(down, prev=T)
+                Q.put(T.down)
+                loc_list.append(down)
+        except:
+            pass
+            
+        try:
+            if M[left.x][left.y] != 1 and (left.x >= 0 and left.y >= 0) and left not in loc_list:
+                T.left = Tree(left, prev=T)
+                Q.put(T.left)
+                loc_list.append(left)
+        except:
+            pass
+            
+        try:
+            if M[right.x][right.y] != 1 and (right.x >= 0 and right.y >= 0) and right not in loc_list:
+                T.right = Tree(right, prev=T)
+                Q.put(T.right)
+                loc_list.append(right)
+        except:
+            pass
+        
+    return T_H
+        
+    
+def to_loc_list(locs):
+    loc_list = []
+    
+    for l in locs:
+        loc_list.append(Loc(l['x'],l['y']))
+        
+    return loc_list
+    
+    
+    
+#       up        right    down     left
+dirs = [Loc(-1,0),Loc(0,1),Loc(1,0),Loc(0,-1)]
+global M
+global H
+global W
+M = []
+
+data = bottle.request.json
+    
+H, W = data["board"]["height"], data["board"]["height"]
 # =============================================================================
 # 
 # print("initializing")
